84fc1e2f873595610815c65dabca3fbf2f2fe055
David Cox <losttime.shuffle@gmail.com>
2015-09-03 22:24:38 -0600

    Update graph with transitions; tooltips on hover

--- src/app/components/meteorite/directive.js
+++ src/app/components/meteorite/directive.js
@@ -11,7 +11,6 @@ meteoriteModule.directive('meteoriteChart', ['meteoriteData', function(meteorite
 		controllerAs: 'ctrl',
 		controller: function($window, $element) {
 			var d3 = $window.d3;
-			console.log('d3', d3);
 			var scope = this;
 
 			this.vm = {};
@@ -24,9 +23,48 @@ meteoriteModule.directive('meteoriteChart', ['meteoriteData', function(meteorite
 			var rawSvg = $element.find("svg")[0];
 			var svg = d3.select(rawSvg);
 
-			var updateChart = function(dataset) {
-				$element.find('svg').children().remove();
+			var xScale, yScale;
 
+			var xAxis = svg.append("g")
+				.attr("class","x axis")
+				.attr("transform","translate(85,470)");
+
+			var yAxis = svg.append("g")
+				.attr("class","y axis")
+				.attr("transform","translate(75,10)");
+
+			var bars = svg.append("g")
+				.attr("class","bars")
+				.attr("transform","translate(85,10)");
+
+			var tip = svg.append('g')
+				.attr('class','tip')
+				.attr("transform","translate(-100,-100)");
+			tip.append('rect')
+				.attr('fill', 'grey')
+				.attr('width', 24)
+				.attr('height', 24);
+			tip.append('text')
+				.attr('x', 12)
+				.attr('y', 17)
+				.attr('width', 24)
+				.attr('text-anchor', 'middle');
+
+			var popTip = function(d) {
+				var tip = svg.select('.tip');
+				if (d) {
+					var x = 80 + xScale(new Date(d.key));
+					var y = d3.min([459, yScale(d.values.length)]);
+
+					tip.attr('transform', 'translate('+x+','+y+')');
+					tip.select('text')
+						.text(d.values.length);
+				} else {
+					tip.attr('transform', 'translate(-100, -100)');
+				}
+			};
+
+			var updateChart = function(dataset) {
 				var nestedData = d3.nest()
 					.key(function(d) {
 						return new Date(d.year);
@@ -34,7 +72,7 @@ meteoriteModule.directive('meteoriteChart', ['meteoriteData', function(meteorite
 					.sortKeys(d3.ascending)
 					.entries(dataset);
 
-				var xScale = d3.time.scale()
+				xScale = d3.time.scale()
 					.domain([d3.min(nestedData, function(d) {
 						return new Date(d.key);
 					}), d3.max(nestedData, function(d) {
@@ -42,7 +80,7 @@ meteoriteModule.directive('meteoriteChart', ['meteoriteData', function(meteorite
 					})])
 					.rangeRound([0, 815]);
 
-				var yScale = d3.scale.linear()
+				yScale = d3.scale.linear()
 					.domain([0,
 						d3.max(nestedData, function(d) {
 							return d.values.length;
@@ -50,62 +88,74 @@ meteoriteModule.directive('meteoriteChart', ['meteoriteData', function(meteorite
 					])
 					.range([460, 0]);
 
-				var xAxis = d3.svg.axis()
-							   .scale(xScale)
-							   .orient("bottom")
-							   .ticks(10);
+				var xAxisConfig = d3.svg.axis()
+					.scale(xScale)
+					.orient("bottom")
+					.ticks(10);
 
-				var yAxis = d3.svg.axis()
-							   .scale(yScale)
-							   .orient("left")
-							   .ticks(10);
+				var yAxisConfig = d3.svg.axis()
+					.scale(yScale)
+					.orient("left")
+					.ticks(10);
 
-				svg.append("g")
-					.attr("class","x axis")
-					.attr("transform","translate(85,470)")
-					.call(xAxis);
+				xAxis.call(xAxisConfig);
+				yAxis.call(yAxisConfig);
 
-				svg.append("g")
-					.attr("class","y axis")
-					.attr("transform","translate(85,10)")
-					.call(yAxis);
+				var rects = bars.selectAll('rect').data(nestedData);
+				rects.enter()
+					.append('rect')
+					.attr('x', function(d) {
+						return xScale(new Date(d.key));
+					})
+					.attr('y', function(d) {
+						return d3.min([459, yScale(d.values.length)]);
+					})
+					.attr('data', function(d) {
+						return d;
+					})
+					.attr('width', 15)
+					.attr('height', function(d) {
+						return 460 - d3.min([459, yScale(d.values.length)]);
+					})
+					.attr('stroke', 'white')
+					.on('mouseover', function(d) {
+						popTip(d);
+					})
+					.on('mouseout', function(d) {
+						popTip();
+					});
+				rects.exit()
+					.remove();
 
-				var rects = svg.selectAll('rect')
-					.data(nestedData)
-					.enter()
-						.append('rect')
-						.attr('x', function(d) {
-							return 85 + xScale(new Date(d.key));
-						})
-						.attr('y', function(d) {
-							return 10 + d3.min([459, yScale(d.values.length)]);
-						})
-						.attr('width', 15)
-						.attr('height', function(d) {
-							return 460 - d3.min([459, yScale(d.values.length)]);
-						})
-						.attr('stroke', 'white');
-						// .style('fill-opacity', 1);
+				rects.transition()
+					.duration(500)
+					.attr('x', function(d) {
+						var x = xScale(new Date(d.key));
+						return x;
+					})
+					.attr('y', function(d) {
+						return d3.min([459, yScale(d.values.length)]);
+					})
+					.attr('height', function(d) {
+						return 460 - d3.min([459, yScale(d.values.length)]);
+					});
 			};
 
 			var getData = function(params) {
 				params = params || {};
-				// params.$where = 'year between \'' + scope.vm.start + '-01-01T00:00:00\' and \'' + scope.vm.end + '-12-31T23:59:59\'';
 				params.$where = 'year >= \'' + scope.vm.start + '-01-01T00:00:00\' AND year <= \'' + scope.vm.end + '-12-31T23:59:59\'';
 				params.$order = 'year';
 				params.fall = 'Fell';
 
 				meteoriteData.getData(params)
 					.then(function(data) {
-						console.log('data', data);
 						updateChart(data);
 					}, function(error) {
-						console.log('error', error);
+						console.error('error', error);
 					});
 			};
 
 			this.vm.dateRangeChanged = function() {
-				console.log('date range changed');
 				getData();
 			};
 
